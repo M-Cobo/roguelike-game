e69d451ebbd6e75be8597d7558cb031757a6031b
Manuel Cobo <yahilcobo@hotmail.com>
2022-12-12 16:15:30 -0400

    Add basic map builder function.

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,40 @@ struct State {
     ecs: World
 }
 
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    return (y as usize * 80) + x as usize;
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80 * 50];
+
+    // Make the boundaries walls
+    for x in 0..80 {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, 49)] = TileType::Wall;
+    }
+
+    for y in 0..50 {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
+
+    // Now we'll randomly splat a bunch of walls.
+    // First, obtain the thread-local RNG:
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    for _i in 0..400 {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let idx = xy_idx(x, y);
+        if idx != xy_idx(40, 25) {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    return map;
+}
+
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World)
 {
     let mut positions = ecs.write_storage::<Position>();
@@ -88,6 +122,8 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();
+
+    gs.ecs.insert(new_map());
 
     gs.ecs
         .create_entity()
