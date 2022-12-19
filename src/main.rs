177361f044921183ca795880e0beff4418122b2a
Manuel Cobo <yahilcobo@hotmail.com>
2022-12-19 11:39:21 -0400

    Change components and player to separate files.

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,17 @@
-use rltk::{ Rltk, GameState, RGB, VirtualKeyCode };
+use rltk::{ Rltk, GameState, RGB };
 use specs::prelude::*;
-use std::cmp::{ max, min };
-use specs_derive::Component;
 
+mod components;
+pub use components::*;
 mod map;
 pub use map::*;
+mod player;
+pub use player::*;
 mod rect;
 pub use rect::Rect;
 
-#[derive(Component)]
-struct Position  {
-    x : i32,
-    y : i32,
-}
-
-#[derive(Component)]
-struct Renderable {
-    glyph: rltk::FontCharType,
-    fg: RGB,
-    bg: RGB,
-}
-
-#[derive(Component)]
-struct Player {}
-
-struct State {
+pub struct State {
     ecs: World
-}
-
-fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World)
-{
-    let mut positions = ecs.write_storage::<Position>();
-    let mut players = ecs.write_storage::<Player>();
-    let map = ecs.fetch::<Vec<TileType>>();
-
-    for (_player, pos) in (&mut players, &mut positions).join() {
-        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
-        if map[destination_idx] != TileType::Wall {        
-            pos.x = min(79, max(0, pos.x + delta_x));
-            pos.y = min(79, max(0, pos.y + delta_y));
-        }
-    }
-}
-
-fn player_input(gs: &mut State, ctx: &mut Rltk)
-{
-    match ctx.key {
-        None => {}
-        Some(key) => match key {
-            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
-            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
-            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
-            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
-            _ => {}
-        }
-    }
 }
 
 impl GameState for State {
